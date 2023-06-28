92bda89dcd00df7d380b59bf3652c80177c5cfba
Mvalidoj <mvalidoj@gmail.com>
2023-06-28 15:19:16 -0400

    Añadidas imágenes para intro slider

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -1,6 +1,11 @@
 class AppImages{
   static String get _package => 'assets/images';
 
+  ///Intro Slider images
+  static String get intro1 => '$_package/intro1.png';
+  static String get intro2 => '$_package/intro2.png';
+  static String get intro3 => '$_package/intro3.png';
+
   static String get splashLogo => '$_package/splash_logo.png';
   static String get registerOnProcess => '$_package/register_on_process.png';
 }
