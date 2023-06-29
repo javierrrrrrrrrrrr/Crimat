9e3a0f581458fb6a2fc665139bd3ac6a1ebe9f59
Mvalidoj <mvalidoj@gmail.com>
2023-06-28 20:00:11 -0400

    Personalizada la barra de navegacion

--- lib/src/features/layout/layout_screen.dart
+++ lib/src/features/layout/layout_screen.dart
@@ -1,3 +1,5 @@
+import 'package:crimat_app/resources/app_icons.dart';
+import 'package:crimat_app/resources/general_styles.dart';
 import 'package:crimat_app/src/shared/extensions/context_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,15 +18,19 @@ class LayoutScreen extends StatelessWidget {
       child: BlocBuilder<LayoutCubit, LayoutState>(
         builder: (context, state) {
           return Scaffold(
-            appBar: AppBar(title: Text(context.loc.app_title,), centerTitle: true,),
             body: selectedScreen(state.selectedIndex),
             bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              elevation: 0,
               currentIndex: state.selectedIndex,
               showSelectedLabels: false,
               showUnselectedLabels: false,
-              items: const [
-                BottomNavigationBarItem(icon: Icon(Icons.home), label: '', ),
-                BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ''),
+              items: [
+                BottomNavigationBarItem(icon: Image.asset(state.selectedIndex == 0 ? AppIcons.homeSelected : AppIcons.home), label: context.loc.home,),
+                BottomNavigationBarItem(icon: Image.asset(state.selectedIndex == 1 ? AppIcons.historySelected : AppIcons.history), label: context.loc.history),
+                BottomNavigationBarItem(icon: buyingCar(3, state.selectedIndex), label: context.loc.buy),
+                BottomNavigationBarItem(icon: Image.asset(state.selectedIndex == 3 ? AppIcons.favoriteSelected : AppIcons.favorite), label: context.loc.favorites),
+                BottomNavigationBarItem(icon: Image.asset(state.selectedIndex == 4 ? AppIcons.profileSelected : AppIcons.profile), label: context.loc.profile),
               ],
               onTap: (newIndex){
                 BlocProvider.of<LayoutCubit>(context).changeScreen(newIndex);
@@ -38,14 +44,42 @@ class LayoutScreen extends StatelessWidget {
 
   Widget selectedScreen(int selectedIndex){
     switch(selectedIndex){
-      case 1:
+      case 0:
         return Center(
           child: Text('HOME', style: TextStyle(fontSize: 32.sp, color: Colors.black),),
+        );
+      case 1:
+        return Center(
+          child: Text('Historial', style: TextStyle(fontSize: 32.sp, color: Colors.black),),
+        );
+      case 2:
+        return Center(
+          child: Text('Carrito', style: TextStyle(fontSize: 32.sp, color: Colors.black),),
         );
       default:
         return Center(
           child: Text('PAGE', style: TextStyle(fontSize: 32.sp, color: Colors.black),),
         );
     }
+  }
+
+  Widget buyingCar(int elements, int selectedIndex){
+    bool isSelected = selectedIndex == 2;
+    return Stack(
+      children: [
+        Image.asset(isSelected ? AppIcons.buySelected : AppIcons.buy),
+        Positioned(
+          top: 0,
+          right: 0,
+          child: Container(
+            alignment: Alignment.center,
+            width: 12,
+            height: 12,
+            decoration: BoxDecoration(color: isSelected ? Colors.white : GStyles.primaryColor, borderRadius: BorderRadius.circular(50), border: Border.fromBorderSide(BorderSide(color: GStyles.primaryColor))),
+            child: Text(elements.toString(), style: GStyles.normalTextStyle.copyWith(color: isSelected ? GStyles.primaryColor : Colors.white, fontSize: 10, fontWeight: FontWeight.bold),),
+          ),
+        )
+      ],
+    );
   }
 }
