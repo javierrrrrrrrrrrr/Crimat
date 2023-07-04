7061d9578f489020dc9c495fad3e2f312cec1097
javier <javierserrano98050602@gmail.com>
2023-07-04 12:47:14 -0400

    grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:crimat_app/src/app/app.dart';
 import 'package:crimat_app/src/shared/dependency_injection/dependency_injection.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await init();
+
   runApp(const MyApp());
 }
