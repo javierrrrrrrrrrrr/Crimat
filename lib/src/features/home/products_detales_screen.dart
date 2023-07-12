33925b83f9b7c84132c8575905bf1d3c18dc8513
javier <javierserrano98050602@gmail.com>
2023-07-12 13:54:42 -0400

    sss

--- lib/src/features/home/products_detales_screen.dart
+++ lib/src/features/home/products_detales_screen.dart
@@ -2,7 +2,6 @@ import 'package:crimat_app/src/features/home/presentation/view/widget/products_d
 import 'package:crimat_app/src/features/home/presentation/view/widget/products_details_widgets/products_details_colum.dart';
 import 'package:crimat_app/src/features/home/presentation/view/widget/products_details_widgets/products_details_header.dart';
 import 'package:crimat_app/src/features/home/presentation/view/widget/products_details_widgets/products_picture_container.dart';
-import 'package:crimat_app/src/models/producto_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -16,8 +15,8 @@ class ProductsDetails extends StatelessWidget {
     final Map<String, dynamic> queryParams =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
 
-    final ProductModel producto =
-        ModalRoute.of(context)?.settings.arguments as ProductModel;
+    // final ProductModel producto =
+    //     ModalRoute.of(context)?.settings.arguments as ProductModel;
 
     return Padding(
       padding: EdgeInsets.only(top: 70.h),
