3b5249e2403371a469bb7e3e6fd4fce762b55a9f
javier <javierserrano98050602@gmail.com>
2023-07-20 10:27:26 -0400

    trabajando en el check

---
+++ lib/src/features/shoppping_cart/presentation/bloc/cart_bloc/cart_state.dart
@@ -0,0 +1,11 @@
+part of 'cart_bloc.dart';
+
+@freezed
+abstract class CartState with _$CartState {
+  const factory CartState.initial() = CartInitial;
+  const factory CartState.loading() = CartLoadingState;
+  const factory CartState.loaded(
+      {required Cart productCartList,
+      required List<bool> checklist}) = CartLoadedState;
+  const factory CartState.error({required String message}) = CartErrorState;
+}
--- lib/src/features/shoppping_cart/utils/shopping_card_aux.dart
+++ lib/src/features/shoppping_cart/utils/shopping_card_aux.dart
@@ -4,6 +4,8 @@ import '../../../models/home/products/producto_model.dart';
 
 class Cart extends Equatable {
   final List<ProductModel> product;
+  final List<bool> check = [];
+
   double get subtotal => product.fold(0, (total, current) {
         if (current.basePrice != null) {
           return total + double.parse(current.basePrice);
@@ -12,7 +14,7 @@ class Cart extends Equatable {
         }
       });
 
-  const Cart({required this.product});
+  Cart({required this.product});
 
   Map<ProductModel, int> productQuantity(List<ProductModel> products) {
     Map<ProductModel, int> quantity = {};
@@ -26,6 +28,7 @@ class Cart extends Equatable {
         quantity[product] = 1;
       }
     }
+
     return quantity;
   }
 
