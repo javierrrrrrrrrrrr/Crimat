7fbc299a4f26d3c1b4bb47f24bbb1c26ce247ea1
Javier <javierserrano98050602@gmail.com>
2023-08-06 20:38:22 -0400

    agregando  .env

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:crimat_app/src/app/app.dart';
 import 'package:crimat_app/src/shared/dependency_injection/dependency_injection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async {
+  await dotenv.load(); // Carga las variables de entorno desde el archivo .env
   WidgetsFlutterBinding.ensureInitialized();
+  //la clave publica desde un archivo agragado al git ignore
+  Stripe.publishableKey = dotenv.env['STRIPE_PUBLISHABLE_KEY']!;
 
   await init();
 
