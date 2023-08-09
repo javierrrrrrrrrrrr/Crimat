991879798aa2206674737b35feaf426f7056f319
javier <javierserrano98050602@gmail.com>
2023-08-09 08:46:03 -0400

    ss

--- lib/src/features/payment/presentation/bloc/payment_bloc.dart
+++ lib/src/features/payment/presentation/bloc/payment_bloc.dart
@@ -1,1 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'payment_event.dart';
+part 'payment_state.dart';
+part 'payment_bloc.freezed.dart';
+
+class PaymentBloc extends Bloc<PaymentEvent, PaymentState> {
+  PaymentBloc(super.initialState);
+
+  Future<void> eventHandler(
+    PaymentEvent event,
+    Emitter emit,
+  ) async {}
+}
--- lib/src/features/payment/presentation/bloc/payment_state.dart
+++ lib/src/features/payment/presentation/bloc/payment_state.dart
@@ -1,10 +1,11 @@
-import 'package:equatable/equatable.dart';
+part of 'payment_bloc.dart';
 
-abstract class PaymentState extends Equatable {
-  const PaymentState();
-
-  @override
-  List<Object> get props => [];
+@freezed
+abstract class PaymentState with _$PaymentState {
+  const factory PaymentState.initial() = _Initial;
+  const factory PaymentState.phase1InProgress() = _Phase1InProgressState;
+  const factory PaymentState.phase2InProgress() = _Phase2InProgressState;
+  const factory PaymentState.completed() = _CompletedState;
+  const factory PaymentState.cancelled() = _CancelledState;
+  const factory PaymentState.error({required String message}) = _ErrorState;
 }
-
-class PaymentSheetInitialized extends PaymentState {}
