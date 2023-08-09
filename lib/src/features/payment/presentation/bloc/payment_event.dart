dbdd44ec226b2183ef1c460d5e759574560994b7
javier <javierserrano98050602@gmail.com>
2023-08-09 09:14:25 -0400

    ss

--- lib/src/features/payment/presentation/bloc/payment_event.dart
+++ lib/src/features/payment/presentation/bloc/payment_event.dart
@@ -6,7 +6,11 @@ abstract class PaymentEvent with _$PaymentEvent {
   const factory PaymentEvent.startedPhase1() =_StartedPhase1Event();
   const factory PaymentEvent.startedPhase2() =_StartedPhase2Event();
   const factory PaymentEvent.erroroccurred() =_ErroroccurredEvent();
-  const factory PaymentEvent.completed() =_CompletedEvent();
+  const factory PaymentEvent.completed() = _CompletedEvent();
+
+ 
+
+  
  
  
  
