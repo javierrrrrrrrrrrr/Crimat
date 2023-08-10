2fcc1f492362384f8e62b9048f7439101f69b23e
javier <javierserrano98050602@gmail.com>
2023-08-10 15:44:18 -0400

    trabajando en pagina de direcion y tipo de envio

--- lib/src/features/payment/presentation/view/payment_select_envio_tipe_widget.dart
+++ lib/src/features/payment/presentation/view/payment_select_envio_tipe_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../models/payment/request_data_model.dart';
 import '../../../../models/payment/shipping_model.dart';
-import '../../../home/presentation/view/widget/products_details_widgets/option_buttoms.dart';
+import '../../../../shared/widgets/cusotm_buttom_product.dart';
+import '../../../perfil/presentation/bloc/profile_bloc.dart';
+import '../../../perfil/presentation/view/widget/custom_delivery_card.dart';
 import '../../../shoppping_cart/presentation/bloc/cart_bloc/cart_bloc.dart';
 import '../../../shoppping_cart/presentation/bloc/check_bloc/check_bloc.dart';
 import '../../../shoppping_cart/utils/shopping_card_aux.dart';
@@ -22,50 +25,102 @@ class TipoEnvioDireccion extends StatelessWidget {
     final paymentbloc = context.read<PaymentBloc>();
     final checkbloc = context.read<CheckBloc>();
     final cartbloc = context.read<CartBloc>();
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(data[0].nombre),
-        OptionButtoms(
-          onPressedPraimary: () async {
-            final Cart cart = Cart(product: cartbloc.productList);
-            for (int i = 0;
-                i < cart.productQuantity(cart.product).keys.length;
-                i++) {
-              productoslistcart.add(
-                ProductoCantidadModel(
-                    producto:
-                        cart.productQuantity(cart.product).keys.elementAt(i).id,
-                    cantidad:
-                        cart.productQuantity(cart.product).values.elementAt(i)),
-              );
-            }
-
-            List<ProductoCantidadModel> auxproductoslistcart = [];
-            for (int i = 0; i < checkbloc.checklist.length - 1; i++) {
-              if (checkbloc.checklist[i] == true) {
-                auxproductoslistcart.add(productoslistcart[i]);
-              }
-            }
-            RequestModel fillRequestModel() {
-              int almacen = cartbloc.productList[0].idAlmacen;
-              int tipoEnvio = 1;
-              List<int> direcciones = [5];
-
-              return RequestModel(
-                productos: auxproductoslistcart,
-                almacen: almacen,
-                tipoEnvio: tipoEnvio,
-                direcciones: direcciones,
-              );
-            }
-
-            paymentbloc
-                .add(PaymentEvent.startedPhase1(datos: fillRequestModel()));
-          },
-          isProductdetails: true,
-        ),
+        const CustomDireccionSelection(),
+        //pasar,le la direcion y el tipo de envio
+        CustomOptionButtom(
+            cartbloc: cartbloc,
+            productoslistcart: productoslistcart,
+            checkbloc: checkbloc,
+            paymentbloc: paymentbloc),
       ],
+    );
+  }
+}
+
+class CustomDireccionSelection extends StatelessWidget {
+  const CustomDireccionSelection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {
+      return state.maybeWhen(
+        orElse: () => Container(),
+        success: (profile) => Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
+          child: CustomDeliveryCard(
+            isCheckout: true,
+            datos: profile,
+            index: 0,
+          ),
+        ),
+      );
+    });
+  }
+}
+
+class CustomOptionButtom extends StatelessWidget {
+  const CustomOptionButtom({
+    super.key,
+    required this.cartbloc,
+    required this.productoslistcart,
+    required this.checkbloc,
+    required this.paymentbloc,
+  });
+
+  final CartBloc cartbloc;
+  final List<ProductoCantidadModel> productoslistcart;
+  final CheckBloc checkbloc;
+  final PaymentBloc paymentbloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return CusotmButtom(
+      onPressed: () {
+        final Cart cart = Cart(product: cartbloc.productList);
+        for (int i = 0;
+            i < cart.productQuantity(cart.product).keys.length;
+            i++) {
+          productoslistcart.add(
+            ProductoCantidadModel(
+                producto:
+                    cart.productQuantity(cart.product).keys.elementAt(i).id,
+                cantidad:
+                    cart.productQuantity(cart.product).values.elementAt(i)),
+          );
+        }
+
+        List<ProductoCantidadModel> auxproductoslistcart = [];
+        for (int i = 0; i < checkbloc.checklist.length - 1; i++) {
+          if (checkbloc.checklist[i] == true) {
+            auxproductoslistcart.add(productoslistcart[i]);
+          }
+        }
+        RequestModel fillRequestModel() {
+          int almacen = cartbloc.productList[0].idAlmacen;
+          int tipoEnvio = 1;
+          List<int> direcciones = [5];
+
+          return RequestModel(
+            productos: auxproductoslistcart,
+            almacen: almacen,
+            tipoEnvio: tipoEnvio,
+            direcciones: direcciones,
+          );
+        }
+
+        paymentbloc.add(PaymentEvent.startedPhase1(datos: fillRequestModel()));
+      },
+      height: 45.h,
+      width: 380.w,
+      name: "Continuar",
+      ispraimary: true,
     );
   }
 }
