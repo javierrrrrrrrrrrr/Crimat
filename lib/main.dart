16c0111f7e4fc2527f02d1df00d5d8606e8b038d
Javier <javierserrano98050602@gmail.com>
2023-08-14 17:50:39 -0400

    bug 3 corregido

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:crimat_app/src/app/app.dart';
 import 'package:crimat_app/src/shared/dependency_injection/dependency_injection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -10,6 +11,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await init();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, // Orientación vertical hacia arriba
+    DeviceOrientation.portraitDown, // Orientación vertical hacia abajo
+  ]);
   // Carga las variables de entorno desde el archivo .env
   await dotenv.load(fileName: ".env");
   Stripe.publishableKey =
